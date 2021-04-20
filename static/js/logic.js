9007f4d40fc8cdfa0aff595b59e2aebafb97d57e
Ruth Hinkle <ruth.e.hinkle@gmail.com>
2021-04-19 20:41:16 -0500

    logic.js- remove out-of-order and coming soon stations, simplified markers, updated filter and legend code

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,11 +5,9 @@ var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 
 // Initialize all the LayerGroups that we'll use.
 var layers = {
-  COMING_SOON: new L.LayerGroup(),
   EMPTY: new L.LayerGroup(),
   LOW: new L.LayerGroup(),
   NORMAL: new L.LayerGroup(),
-  OUT_OF_ORDER: new L.LayerGroup()
 };
 
 // Create the map with our layers.
@@ -17,11 +15,9 @@ var map = L.map("map-id", {
   center: [41.88, -87.62],
   zoom: 11,
   layers: [
-    layers.COMING_SOON,
     layers.EMPTY,
     layers.LOW,
     layers.NORMAL,
-    layers.OUT_OF_ORDER
   ]
 });
 
@@ -30,11 +26,9 @@ streetmap.addTo(map);
 
 // Create an overlays object to add to the layer control.
 var overlays = {
-  "Coming Soon": layers.COMING_SOON,
   "Empty Stations": layers.EMPTY,
   "Low Stations": layers.LOW,
-  "Healthy Stations": layers.NORMAL,
-  "Out of Order": layers.OUT_OF_ORDER
+  "Full Stations": layers.NORMAL,
 };
 
 // Create a control for our layers, and add our overlays to it.
@@ -55,42 +49,30 @@ info.addTo(map);
 
 // Initialize an object that contains icons for each layer group.
 var icons = {
-  COMING_SOON: L.ExtraMarkers.icon({
-    icon: "ion-settings",
-    iconColor: "white",
-    markerColor: "yellow",
-    shape: "star"
-  }),
   EMPTY: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
+    // icon: "ion-android-bicycle",
     iconColor: "white",
     markerColor: "red",
     shape: "circle"
   }),
-  OUT_OF_ORDER: L.ExtraMarkers.icon({
-    icon: "ion-minus-circled",
-    iconColor: "white",
-    markerColor: "blue-dark",
-    shape: "penta"
-  }),
   LOW: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
+    // icon: "ion-android-bicycle",
     iconColor: "white",
     markerColor: "orange",
     shape: "circle"
   }),
   NORMAL: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
+    // icon: "ion-android-bicycle",
     iconColor: "white",
     markerColor: "green",
     shape: "circle"
   })
 };
 
-// Perform an API call to the Citi Bike station information endpoint.
+// Perform an API call to the Divvy Bike station information endpoint.
 d3.json("https://gbfs.divvybikes.com/gbfs/en/station_information.json").then(function(infoRes) {
 
-  // When the first API call completes, perform another call to the Citi Bike station status endpoint.
+  // When the first API call completes, perform another call to the Divvy Bike station status endpoint.
   d3.json("https://gbfs.divvybikes.com/gbfs/en/station_status.json").then(function(statusRes) {
     var updatedAt = infoRes.last_updated;
     var stationStatus = statusRes.data.stations;
@@ -98,11 +80,9 @@ d3.json("https://gbfs.divvybikes.com/gbfs/en/station_information.json").then(fun
 
     // Create an object to keep the number of markers in each layer.
     var stationCount = {
-      COMING_SOON: 0,
       EMPTY: 0,
       LOW: 0,
       NORMAL: 0,
-      OUT_OF_ORDER: 0
     };
 
     // Initialize stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station count for the layer group.
@@ -113,18 +93,12 @@ d3.json("https://gbfs.divvybikes.com/gbfs/en/station_information.json").then(fun
 
       // Create a new station object with properties of both station objects.
       var station = Object.assign({}, stationInfo[i], stationStatus[i]);
-      // If a station is listed but not installed, it's coming soon.
-      if (!station.is_installed) {
-        stationStatusCode = "COMING_SOON";
-      }
+
       // If a station has no available bikes, it's empty.
-      else if (!station.num_bikes_available) {
+      if (!station.num_bikes_available) {
         stationStatusCode = "EMPTY";
       }
-      // If a station is installed but isn't renting, it's out of order.
-      else if (station.is_installed && !station.is_renting) {
-        stationStatusCode = "OUT_OF_ORDER";
-      }
+      
       // If a station has less than five bikes, it's status is low.
       else if (station.num_bikes_available < 5) {
         stationStatusCode = "LOW";
@@ -156,11 +130,10 @@ d3.json("https://gbfs.divvybikes.com/gbfs/en/station_information.json").then(fun
 // Update the legend's innerHTML with the last updated time and station count.
 function updateLegend(time, stationCount) {
   document.querySelector(".legend").innerHTML = [
-    "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
-    "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
-    "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
-    "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
-    "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
-    "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
+    "<p><strong>Station Bike Capacity</strong></p>",
+        "<p class='empty'>Empty: " + stationCount.EMPTY + "</p>",
+    "<p class='low'>Low: " + stationCount.LOW + "</p>",
+    "<p class='full'>Full: " + stationCount.NORMAL + "</p>",
+    "<p><em>Updated: " + moment.unix(time).format("h:mm:ss A") + "</em></p>"
   ].join("");
 }
